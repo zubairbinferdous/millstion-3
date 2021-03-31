88519b6b2d89688228a36a9d493520e7dd963673
zubair bin ferdous <zubairbinf@gmail.com>
2021-03-31 23:50:56 +0600

    ok

---
+++ m-15/random.js
@@ -0,0 +1,12 @@
+var num = 2.5555555;
+var result = Math.floor(num);
+var result1 = Math.floor(num);
+var result2 = Math.floor(num);
+
+for (let i = 0; i < 3; i++) {
+    
+    var random = Math.random() * 6;
+    var ludo = Math.round(random);
+    console.log(ludo);
+    
+}
---
+++ m-15/swap.js
@@ -0,0 +1,23 @@
+var a = 5;
+var b = 8;
+console.log('before swap : a = ' ,a , 'b = ' , b);
+temp = a ;
+ a = b ;
+ b = temp; 
+ console.log('after swap : a = ' ,a , 'b = ' , b);
+
+
+
+ var x = 10;
+ var y = 20;
+ x = x + y;
+ y = x - y;
+ x = x - y;
+ console.log('after swap : x = ' ,x , 'y = ' , y);
+
+
+ var r = 5;
+ var u = 10;
+
+ [ r , u] = [u , r];
+ console.log('after swap : u = ' ,u , 'r = ' , r);
