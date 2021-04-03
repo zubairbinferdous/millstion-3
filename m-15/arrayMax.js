8a4ccd714b2f282f83f9480723af230a0083b170
zubair bin ferdous <zubairbinf@gmail.com>
2021-04-04 01:18:57 +0600

    ok done

---
+++ m-15/arrayMax.js
@@ -0,0 +1,12 @@
+var mark = [44,4,66,78,90,35,67,98,88,99];
+var max = [];
+for (let i = 0; i < mark.length; i++) {
+    const element = mark[i];
+
+    if ( element > max) {
+        max = element;
+    }
+    
+}
+
+console.log('hight number :' , max)
--- m-15/max.js
+++ m-15/max.js
@@ -1,4 +1,25 @@
-var business = 200;
-var minister = 350;
+var business = 500;
+var minister = 450;
 var bcs      = 300;
 
+if ( business > minister) {
+     if ( business > bcs) {
+        console.log("Business is big")
+     }else{
+         consol.log('bcs is bigger');
+     }
+}else{
+    if ( minister > bcs) {
+
+        console.log("minster is big")
+        
+    }else{
+        consol.log('bcs is big')
+    }
+}
+
+
+// **************************
+
+var max = Math.max(business,bcs,minister);
+console.log(max);
