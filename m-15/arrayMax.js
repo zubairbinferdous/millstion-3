494b7802d3ba4c1d2616ef2c0d2c531955b7e43a
zubair bin ferdous <zubairbinf@gmail.com>
2021-04-04 15:20:39 +0600

    ok this is done

--- m-15/arrayMax.js
+++ m-15/arrayMax.js
@@ -1,3 +1,5 @@
+
+
 var mark = [44,4,66,78,90,35,67,98,88,99];
 var max = [];
 for (let i = 0; i < mark.length; i++) {
@@ -9,4 +11,4 @@ for (let i = 0; i < mark.length; i++) {
     
 }
 
-console.log('hight number :' , max)
+console.log('hight number :', max)
